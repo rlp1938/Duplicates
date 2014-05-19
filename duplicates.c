17968a37e9790ec53d56b70a63cd9326cc09321f
Bob Parker <rlp1938@gmail.com>
2014-05-19 20:18:55 +0700

    Initial load of what I believe is the minimum set of files to have the program ./configure && nmake etc and also comply with GNU coding standards.

---
+++ duplicates.c
@@ -0,0 +1,612 @@
+
+/*      duplicates.c
+ *
+ *	Copyright 2011 Bob Parker <rlp1938@gmail.com>
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *	MA 02110-1301, USA.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+#include <errno.h>
+#include <dirent.h>
+#include "md5.h"
+#include "config.h"
+
+int filecount;
+
+struct filedata {
+    char *from; // start of file content
+    char *to;   // last byte of data + 1
+};
+
+struct hashrecord {
+	char thesum[33];
+	ino_t ino;
+	char ftyp;
+	char path[PATH_MAX];
+};
+
+void help_print(int forced);
+FILE *dofopen(const char *path, const char *mode);
+char *dostrdup(const char *s);
+void recursedir(char *headdir, FILE *fpo);
+char *domd5sum(const char *pathname);
+struct hashrecord parse_line(char *line);
+void clipeol(char *line);
+char *thepathname(char *line);
+char *getcluster(char *path, int depth);
+
+
+
+char *helptext = "\n\tUsage: duplicates [option] dir_to_search\n"
+  "\n\tOptions:\n"
+  "\t-h outputs this help message.\n"
+  "\t-d debug mode, don't discard temporary work files.\n"
+  "\t-n report hard linked duplicates as well as separate files.\n"
+  ;
+
+
+char *eol; // string in case I ever want to do Microsoft
+int clusterdepth;
+
+
+int main(int argc, char **argv)
+{
+	int opt, result, verbosity, delworks;
+	struct stat sb;
+	char *workfile0;
+	char *workfile1;
+	char *workfile2;
+	char *workfile3;
+	char *workfile4;
+	char *workfile5;
+	char *workfile6;
+	char *workfile7;
+	char command[FILENAME_MAX];
+	FILE *fpo, *fpi, *fpofinal;
+	char *line1, *line2, *line3;
+	char buf1[PATH_MAX*2], buf2[PATH_MAX], clustername[PATH_MAX];
+	struct hashrecord hr1, hr2;
+	char *topdir;
+	char *fgetsresult;
+
+	// set default values
+	verbosity = 0;
+	filecount = 0;
+	fpofinal = stdout;
+	clusterdepth = 3;
+	delworks = 1;	// delete workfiles is the default
+
+	eol = "\n";	// string in case I ever want to do Microsoft
+
+	while((opt = getopt(argc, argv, ":hvdc:")) != -1) {
+		switch(opt){
+		case 'h':
+			help_print(0);
+		break;
+		case 'd':
+			delworks = 0;	// don't trash the workfiles at end
+		break;
+		case 'c':
+			clusterdepth = atoi(optarg);
+		break;
+		case 'v':
+			verbosity++;	// 4 levels of verbosity, 0-3. 0 no progress
+							// report, 1 print every 100th pathname,
+							// 2 every tenth, 3 (and above) print every
+							// pathname.
+		break;
+		case ':':
+			fprintf(stderr, "Option %c requires an argument\n",optopt);
+			help_print(1);
+		break;
+		case '?':
+			fprintf(stderr, "Illegal option: %c\n",optopt);
+			help_print(1);
+		break;
+		} //switch()
+	}//while()
+	// now process the non-option arguments
+
+	// 1.Check that argv[???] exists.
+	if (!(argv[optind])) {
+		fprintf(stderr, "No directory provided\n");
+		help_print(1);
+	}
+
+	// 2. Check that the dir exists.
+	if ((stat(argv[optind], &sb)) == -1){
+		perror(argv[optind]);
+		help_print(EXIT_FAILURE);
+	}
+
+	// 3. Check that this is a dir
+	if (!(S_ISDIR(sb.st_mode))) {
+		fprintf(stderr, "Not a directory: %s\n", argv[optind]);
+		help_print(EXIT_FAILURE);
+	}
+
+	// generate my workfile names
+	sprintf(command, "/tmp/%sduplicates0", getenv("USER"));
+	workfile0 = dostrdup(command);
+	sprintf(command, "/tmp/%sduplicates1", getenv("USER"));
+	workfile1 = dostrdup(command);
+	sprintf(command, "/tmp/%sduplicates2", getenv("USER"));
+	workfile2 = dostrdup(command);
+	sprintf(command, "/tmp/%sduplicates3", getenv("USER"));
+	workfile3 = dostrdup(command);
+	sprintf(command, "/tmp/%sduplicates4", getenv("USER"));
+	workfile4 = dostrdup(command);
+	sprintf(command, "/tmp/%sduplicates5", getenv("USER"));
+	workfile5 = dostrdup(command);
+	sprintf(command, "/tmp/%sduplicates6", getenv("USER"));
+	workfile6 = dostrdup(command);
+	sprintf(command, "/tmp/%sduplicates7", getenv("USER"));
+	workfile7 = dostrdup(command);
+
+
+	// Step 0 - list the files
+	if (verbosity){
+		fputs("Generating list of files and symlinks\n",stderr);
+	}
+	topdir = argv[optind];
+	{
+		// get rid of trailing '/'
+		int len = strlen(topdir);
+		if (topdir[len-1] == '/') topdir[len-1] = '\0';
+	}
+	fpo = dofopen(workfile0, "w");
+	recursedir(topdir, fpo);
+	fclose(fpo);
+	// Now sort them
+	if (verbosity){
+		fputs("Soting list of files\n", stderr);
+	}
+	if (setenv("LC_ALL", "C", 1) == -1){	// sort bitwise L-R
+		perror("LC_ALL=C");
+		exit(EXIT_FAILURE);
+	}
+	sprintf(command, "sort %s > %s",
+						workfile0, workfile1);
+	result = system(command);
+	if(result == -1){
+		perror(command);
+		exit(EXIT_FAILURE);
+	}
+
+	// Step1: Traverse the list of files in workfile1 calculating MD5
+	if (verbosity){
+		fputs("Calculating md5sums\n", stderr);
+	}
+	fpi = dofopen(workfile1, "r");
+	fpo = dofopen(workfile2, "w");
+	while (fgets(buf1, PATH_MAX, fpi)){
+		struct stat sb;
+		clipeol(buf1);
+		char *cp;
+		//cp = strrchr(buf1, ' ');
+		cp = strstr(buf1, " f");
+		if (!(cp)) cp = strstr(buf1, " s");
+		*cp = '\0';	// end of the file path
+		//fprintf(stderr, "%s\n", buf1);	// TODO - verbosity levels
+		cp++;	// now looking at the file type, s|f
+		if (stat(buf1, &sb) == -1){
+			perror(buf1);	// just report it.
+		} else {
+			int report;
+			char *thesum = domd5sum(buf1);
+			fprintf(fpo, "%s %lu %c %s\n", domd5sum(buf1),
+				sb.st_ino, *cp, buf1);
+			// NB in the case of a symlink, sb.st_ino is the inode of
+			// the target, not the inode of the link itself.
+			filecount++;
+			switch(verbosity) {
+				case 0:	// do nothing
+				case 1:
+				report = 0;
+				break;
+				case 2:	// report every 100th
+				report = (filecount % 100 == 0) ? 1:0;
+				break;
+				case 3:	// report every tenth
+				report = (filecount % 10 == 0) ? 1:0;
+				break;
+				default:	// > 3 report every file
+				report = 1;
+				break;
+			}
+			if(report) {
+				fprintf(stderr, "Processing: %s\n", buf1);
+			}
+		}
+	}
+	fclose(fpi);
+	fclose(fpo);
+
+	// Step 2; sort the resultant file of sums + paths
+	if (verbosity){
+		fputs("Sorting on md5sum, inode, path\n", stderr);
+	}
+
+	sprintf(command, "sort %s > %s",
+						workfile2, workfile3);
+	result = system(command);
+	if(result == -1){
+		perror(command);
+		exit(EXIT_FAILURE);
+	}
+
+	// Step 3 process the sorted work file
+	if (verbosity){
+		fputs("Discarding pathnames of unique files\n", stderr);
+	}
+	fpi = dofopen(workfile3, "r");
+	fpo = dofopen(workfile4, "w");
+	// Looking for duplicates, initialise the loop.
+	fgetsresult = fgets(buf1, PATH_MAX, fpi);
+	if(!(fgetsresult)) {
+		perror("fgets()");
+	} // did this to stop the compiler bitching
+
+	line1 = buf1;
+	line2 = buf2;
+	hr1 = parse_line(line1);
+
+	while (fgets(line2, PATH_MAX, fpi)) {
+		/*
+		 * The job here is to determine what has been duplicated.
+		 * Very simply if the md5sums differ in line1 and line2,
+		 * there is no duplication. Just discard line1.
+		 * OTOH if the 2 md5sums match and also the inode number,
+		 * there is no duplication. (This fits for symlinks and hard
+		 * links). Likewise discard line1.
+		 * But a match on md5sums with no match on inode is a duplicate.
+		 * In this case report them both, then discard line1, and then
+		 * line2 becomes line1, rinse and repeat.
+		 * If we have more than a pair of duplicates, eg 3 called
+		 * instance1, ..2, ..3 for example, there will will 4 output
+		 * records instance1 paired with instance2, then ..2 paired
+		 * with ..3.
+		 * So I follow this with sort -u to discard the repeated ..2.
+		*/
+		hr2 = parse_line(line2);
+		if (strcmp(hr1.thesum, hr2.thesum) == 0) {
+			// maybe a duplicate or just multiple link
+			if (hr1.ino != hr2.ino) {
+				// genuine duplication, write both lines out
+				fprintf(fpo, "%s %lu %c %s\n",
+						hr1.thesum, hr1.ino, hr1.ftyp, hr1.path);
+				fprintf(fpo, "%s %lu %c %s\n",
+						hr2.thesum, hr2.ino, hr2.ftyp, hr2.path);
+
+			}
+		}
+		// at the end, exchange line1 and line2
+		{
+			char *tmp = line1;
+			line1 = line2;
+			line2 = tmp;
+		}
+		hr1 = hr2;
+	}
+	fclose(fpo);
+	fclose(fpi);
+
+	// Step 4, use sort to get rid of duplicated lines
+	if (verbosity){
+		fputs("Discarding duplicated pathnames\n", stderr);
+	}
+	sprintf(command, "sort -u %s > %s",
+						workfile4, workfile5);
+	result = system(command);
+	if(result == -1){
+		perror(command);
+		exit(EXIT_FAILURE);
+	}
+
+	// Step 5, group the duplicates by one pathname
+	if (verbosity){
+		fputs("Setting logical group names on every line\n",
+				stderr);
+	}
+	fpi = dofopen(workfile5, "r");
+	fpo = dofopen(workfile6, "w");
+	line1 = &buf1[0];
+	line2 = &buf2[0];
+	fgetsresult = fgets(line1, PATH_MAX, fpi);
+	if(!(fgetsresult)) {
+		perror("fgets()");
+	} // did this to stop the compiler bitching
+	clipeol(line1);
+	line3 = thepathname(line1);
+	strcpy(clustername, getcluster(line3, clusterdepth));
+
+	while ((fgets(line2, PATH_MAX, fpi))) {
+		char *cp;
+		// I know that these are duplicated file names.
+		clipeol(line2);
+		fprintf(fpo, "%s %s\n", clustername, line1);
+		while (strncmp(line1, line2, 32) == 0) {
+			fprintf(fpo, "%s %s\n", clustername, line2);
+			cp = fgets(line2, PATH_MAX, fpi);
+			if(!(cp)) break;
+			clipeol(line2);
+		}
+		// exchange line1 and line2 now
+		{
+			char *tmp = line1;
+			line1 = line2;
+			line2 = tmp;
+		}
+		// new clustername
+		line3 = thepathname(line1);
+		strcpy(clustername, line3);
+	} // while()
+
+	fclose(fpo);
+	fclose(fpi);
+
+	// Step 6, sort the mess by groupname
+	if (verbosity){
+		fputs("Sort on logical group names.\n",
+				stderr);
+	}
+	sprintf(command, "sort %s > %s",
+						workfile6, workfile7);
+	result = system(command);
+	if(result == -1){
+		perror(command);
+		exit(EXIT_FAILURE);
+	}
+
+	// Step 7, format the results for the user
+	if (verbosity){
+		fputs("Send the results to stdout.\n",
+				stderr);
+	}
+	fpi = dofopen(workfile7, "r");
+	// fpofinal has been dealt with at options processing time.
+	while((fgets(buf1, 2 * PATH_MAX, fpi))) {
+		line1 = strchr(buf1, ' ');	// first space
+		line1++;	// now looking at md5sum
+		hr1 = parse_line(line1);
+		fprintf(fpofinal, "%s %s %lu %c\n",
+					hr1.path, hr1.thesum, hr1.ino, hr1.ftyp);
+	} // while()
+
+	sync();
+	if (delworks){
+		if (verbosity){
+			fputs("Deleting workfiles.\n",
+				stderr);
+		}
+		unlink(workfile0);
+		unlink(workfile1);
+		unlink(workfile2);
+		unlink(workfile3);
+		unlink(workfile4);
+		unlink(workfile5);
+		unlink(workfile6);
+		unlink(workfile7);
+	}
+
+	return 0;
+} // main()
+
+void help_print(int forced){
+    fputs(helptext, stderr);
+    exit(forced);
+} // help_print()
+
+FILE *dofopen(const char *path, const char *mode)
+{
+	// fopen with error handling
+	FILE *fp = fopen(path, mode);
+	if(!(fp)){
+		perror(path);
+		exit(EXIT_FAILURE);
+	}
+	return fp;
+} // dofopen()
+
+
+char *dostrdup(const char *s)
+{
+	/*
+	 * strdup() with in built error handling
+	*/
+	char *cp = strdup(s);
+	if(!(cp)) {
+		perror(s);
+		exit(EXIT_FAILURE);
+	}
+	return cp;
+} // dostrdup()
+
+void recursedir(char *headdir, FILE *fpo)
+{
+	/* open the dir at headdir and process according to file type.
+	*/
+	DIR *dirp;
+	struct dirent *de;
+
+	dirp = opendir(headdir);
+	if (!(dirp)) {
+		perror(headdir);
+		exit(EXIT_FAILURE);
+	}
+	while((de = readdir(dirp))) {
+		if (strcmp(de->d_name, "..") == 0) continue;
+		if (strcmp(de->d_name, ".") == 0) continue;
+
+		switch(de->d_type) {
+			char newpath[FILENAME_MAX];
+			struct stat sb;
+			char ftyp;
+			// Nothing to do for these.
+			case DT_BLK:
+			case DT_CHR:
+			case DT_FIFO:
+			case DT_SOCK:
+			continue;
+			break;
+			case DT_LNK:
+			strcpy(newpath, headdir);
+			strcat(newpath, "/");
+			strcat(newpath, de->d_name);
+			if (stat(newpath, &sb) == -1) {
+				perror(newpath);
+				break;
+			}
+			if (sb.st_size == 0) break;	// no interest in 0 length files
+			ftyp = 's';
+			goto REG_LNK_common;
+			case DT_REG:
+			strcpy(newpath, headdir);
+			strcat(newpath, "/");
+			strcat(newpath, de->d_name);
+			if (stat(newpath, &sb) == -1) {
+				perror(newpath);
+				break;
+			}
+			if (sb.st_size == 0) break;	// no interest in 0 length files
+			ftyp = 'f';
+REG_LNK_common:
+			fprintf(fpo, "%s %c \n", newpath, ftyp );
+			break;
+			case DT_DIR:
+			// recurse using this pathname.
+			strcpy(newpath, headdir);
+			strcat(newpath, "/");
+			strcat(newpath, de->d_name);
+			recursedir(newpath, fpo);
+			break;
+			// Just report the error but nothing else.
+			case DT_UNKNOWN:
+			fprintf(stderr, "Unknown type:\n%s/%s\n\n", headdir,
+					de->d_name);
+			break;
+
+		} // switch()
+	} // while
+	closedir(dirp);
+} // recursedir()
+
+char *domd5sum(const char *pathname)
+{
+	/* calculate md5sum of file in pathname */
+
+	static char c[33];	// length of md5sum string + 1
+	int i, hashsize, bytesread;
+    struct md5_ctx ctx;
+    unsigned char buffer[1048576];
+    unsigned char hash[16];
+    char *tmp;
+    FILE *fpi;
+
+    fpi = dofopen(pathname, "r");
+    c[0] = '\0';
+
+    md5_init_ctx (&ctx);
+
+	while ((bytesread = fread(&buffer, 1, 1048576, fpi)) > 0) {
+		if ((bytesread % 64) == 0) {
+			md5_process_block (&buffer, bytesread, &ctx);
+		} else {
+			md5_process_bytes (&buffer, bytesread, &ctx);
+		}
+	}
+
+	md5_finish_ctx (&ctx, &hash[0]);
+
+	hashsize = 16;
+	tmp = &c[0];
+	/*TODO just unroll the loop below */
+    for (i = 0; i < hashsize; i++) {
+           sprintf(tmp, "%.2x", hash[i]);
+        tmp += 2;
+    }
+    c[32] = '\0';
+	fclose(fpi);
+	return c;
+} // domd5sum()
+
+struct hashrecord parse_line(char *line)
+{
+	/* parse the data contained in line into the struct */
+	char *cp;
+	struct hashrecord hr;
+	/* struct hashrecord {
+	char thesum[33];
+	ino_t ino;
+	char ftyp;
+	char path[PATH_MAX];
+	}; */
+	strncpy(hr.thesum, line, 32);
+	hr.thesum[32] = '\0';
+	cp = line + 34;	// first char of inode (as string).
+	hr.ino = (unsigned long) atol(cp);
+	while (*cp != 'f' && *cp != 's') cp++;	// point to file type
+	hr.ftyp = *cp;
+	cp++;	cp++;	// now looking at the path
+	strcpy(hr.path, cp);
+	cp = strchr(hr.path, '\n');
+	if (cp) *cp = '\0';
+	return hr;
+} // parseline()
+
+void clipeol(char *line)
+{
+	char *cp;
+	cp = strchr(line, '\n');
+	if(cp) {
+		*cp = '\0';
+	}
+} // clipeol()
+
+char *thepathname(char *line)
+{
+	char *cp;
+	cp = strrchr(line, ' ');
+	cp++;	// first non space after
+	return cp;
+} // thepathname()
+
+char *getcluster(char *path, int depth)
+{
+	static char buf[PATH_MAX];
+	char *cp;
+	int target;
+	int count = 0;
+	target = depth -1;
+	strcpy(buf, path);
+	cp = strchr(buf, '\n');
+	if (cp) *cp = '\0';
+	cp = buf;
+	while ((cp = strchr(cp, '/')) && (count < target)) {
+		count++;
+		cp++;
+	}
+	if (cp) *cp = '\0';
+	return buf;
+} // getcluster()
