b8f56422a62f2e72f962b94967cbf4720b283e51
bob <rlp1938@gmail.com>
2015-10-12 14:30:16 +0700

    copyright date updated.

--- duplicates.c
+++ duplicates.c
@@ -1,6 +1,6 @@
 /*      duplicates.c
  *
- *	Copyright 2011 Bob Parker <rlp1938@gmail.com>
+ *	Copyright 2015 Bob Parker <rlp1938@gmail.com>
  *
  *	This program is free software; you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
